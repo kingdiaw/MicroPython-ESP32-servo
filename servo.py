72296bb70d2e01b6e9cc7f0fe392c3e0b857247e
kingdiaw <kingdiawehsut@gmail.com>
2023-07-10 13:58:46 +0800

    Update servo.py
    major change

--- servo.py
+++ servo.py
@@ -1,68 +1,33 @@
-from machine import Pin, PWM
- 
- 
+import machine
+import math
+
 class Servo:
-    """ A simple class for controlling a 9g servo with the Raspberry Pi Pico.
- 
-    Attributes:
- 
-        minVal: An integer denoting the minimum duty value for the servo motor.
- 
-        maxVal: An integer denoting the maximum duty value for the servo motor.
- 
-    """
- 
-    def __init__(self, pin: int or Pin or PWM, minVal=2500, maxVal=7500):
-        """ Creates a new Servo Object.
- 
-        args:
- 
-            pin (int or machine.Pin or machine.PWM): Either an integer denoting the number of the GPIO pin or an already constructed Pin or PWM object that is connected to the servo.
- 
-            minVal (int): Optional, denotes the minimum duty value to be used for this servo.
- 
-            maxVal (int): Optional, denotes the maximum duty value to be used for this servo.
- 
-        """
- 
-        if isinstance(pin, int):
-            pin = Pin(pin, Pin.OUT)
-        if isinstance(pin, Pin):
-            self.__pwm = PWM(pin)
-        if isinstance(pin, PWM):
-            self.__pwm = pin
-        self.__pwm.freq(50)
-        self.minVal = minVal
-        self.maxVal = maxVal
- 
-    def deinit(self):
-        """ Deinitializes the underlying PWM object.
- 
-        """
-        self.__pwm.deinit()
- 
-    def goto(self, value: int):
-        """ Moves the servo to the specified position.
- 
-        args:
- 
-            value (int): The position to move to, represented by a value from 0 to 1024 (inclusive).
- 
-        """
-        if value < 0:
-            value = 0
-        if value > 1024:
-            value = 1024
-        delta = self.maxVal-self.minVal
-        target = int(self.minVal + ((value / 1024) * delta))
-        self.__pwm.duty_u16(target)
- 
-    def middle(self):
-        """ Moves the servo to the middle.
-        """
-        self.goto(512)
- 
-    def free(self):
-        """ Allows the servo to be moved freely.
-        """
-        self.__pwm.duty_u16(0)
+    def __init__(self,pin_id,min_us=544.0,max_us=2400.0,min_deg=0.0,max_deg=180.0,freq=50):
+        self.pwm = machine.PWM(machine.Pin(pin_id))
+        self.pwm.freq(freq)
+        self.current_us = 0.0
+        self._slope = (min_us-max_us)/(math.radians(min_deg)-math.radians(max_deg))
+        self._offset = min_us
+        
+    def write(self,deg):
+        self.write_rad(math.radians(deg))
+
+    def read(self):
+        return math.degrees(self.read_rad())
+        
+    def write_rad(self,rad):
+        self.write_us(rad*self._slope+self._offset)
+    
+    def read_rad(self):
+        return (self.current_us-self._offset)/self._slope
+        
+    def write_us(self,us):
+        self.current_us=us
+        self.pwm.duty_ns(int(self.current_us*1000.0))
+    
+    def read_us(self):
+        return self.current_us
+
+    def off(self):
+        self.pwm.duty_ns(0)
+
